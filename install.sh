72232da21fee05dc95b916f1844306e507559094
ukcastle <jo631@naver.com>
2022-08-03 10:59:43 +0900

    update install to download weight

--- install.sh
+++ install.sh
@@ -4,3 +4,16 @@ pip install -r requirements.txt
 
 wget https://github.com/ukcastle/yolov5_rknn/releases/download/1.7.1/rknn_toolkit-1.7.1-cp36-cp36m-linux_x86_64.whl
 pip install rknn_toolkit-1.7.1-cp36-cp36m-linux_x86_64.whl
+
+python - <<EOF
+from utils.downloads import attempt_download
+
+models = ['n', 's', 'm']
+models.extend([x + '6' for x in models])  # add P6 models
+
+for x in models:
+    attempt_download(f'yolov5{x}.pt')
+
+EOF
+
+mv yolov5*.pt models/
