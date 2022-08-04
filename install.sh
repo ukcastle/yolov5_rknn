44525dc28fff351c9959cb278b41f8b24c39ef42
ukcastle <jo631@naver.com>
2022-08-04 09:37:55 +0900

    update requirments & install sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # conda create -n yolo_rknn python=3.6.13 # Use 3.6!!
 # conda activate yolo_rknn
+conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=11.3 -c pytorch -c conda-forge
 pip install -r requirements.txt
 
 wget https://github.com/ukcastle/yolov5_rknn/releases/download/1.7.1/rknn_toolkit-1.7.1-cp36-cp36m-linux_x86_64.whl
 pip install rknn_toolkit-1.7.1-cp36-cp36m-linux_x86_64.whl
+pip install protobuf==3.12.0
 
 python - <<EOF
 from utils.downloads import attempt_download
